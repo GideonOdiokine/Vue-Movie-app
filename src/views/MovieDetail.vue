78602c7f8e41aba06a96d05ae3f73b158024edad
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2023-06-30 03:36:52 +0100

    Movie Detail Page

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -1,9 +1,67 @@
 <template>
-  <h3>Movie Detail</h3>
+  <div class="movie-detail">
+    <h2>{{ movie.Title }}</h2>
+    <p>{{ movie.Year }}</p>
+    <img :src="movie.Poster" alt="Movie Poster" class="featured-img" />
+    <p>{{ movie.Plot }}</p>
+  </div>
 </template>
 
 <script lang="ts">
-export default {};
+import { defineComponent, ref, onBeforeMount } from "vue";
+import { useRoute } from "vue-router";
+
+export default defineComponent({
+  name: "MovieDetail",
+  components: {},
+  setup() {
+    const movie = ref<{
+      Title?: string;
+      Year?: string;
+      Rated?: string;
+      Plot?: string;
+      Poster?: string;
+    }>({});
+    const route = useRoute();
+
+    onBeforeMount(() => {
+      fetch(
+        `http://www.omdbapi.com?apikey=${process.env.VUE_APP_API_KEY}&i=${route.params.id}&plot=full`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          movie.value = data;
+        });
+    });
+
+    return {
+      movie,
+    };
+  },
+});
 </script>
 
-<style></style>
+<style lang="scss">
+.movie-detail {
+  padding: 16px;
+
+  h2 {
+    color: #fff;
+    font-size: 28px;
+    font-weight: 600;
+    margin-bottom: 16px;
+  }
+
+  .featured-img {
+    display: block;
+    max-width: 200px;
+    margin-bottom: 16px;
+  }
+
+  p {
+    color: #fff;
+    font-size: 18px;
+    line-height: 1.4;
+  }
+}
+</style>
